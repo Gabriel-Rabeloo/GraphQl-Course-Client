e56e44b3b4f6e86072544548a37ecf0671c66069
Gabriel-Rabeloo <gabriiel.rabeloo@gmail.com>
2022-02-26 13:46:27 -0300

    Configurando Apollo Client para React

--- src/page-templates/Login/index.jsx
+++ src/page-templates/Login/index.jsx
@@ -1,12 +1,47 @@
-import { AuthForm } from 'components/AuthForm';
+import { useState } from 'react';
 import { Helmet } from 'react-helmet';
 
+import { useMutation } from '@apollo/client';
+
+import { AuthForm } from 'components/AuthForm';
+import { GQL_LOGIN } from 'graphql/mutations/auth';
+import { Loading } from 'components/Loading';
+
 export const Login = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [login, { loading, error, data }] = useMutation(GQL_LOGIN, {
+    onError() {},
+  });
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    const variables = {
+      userName,
+      password,
+    };
+
+    await login({ variables });
+  };
+
+  if (loading) return <Loading loading={loading} />;
+  // if (error) return <DefaultError error={error} />;
+
+  console.log(data);
+
   return (
     <>
-      <Helmet title="Login - GraphQL + Apollo-Client - Otávio Miranda" />
+      <Helmet title="Login - GraphQL + Apollo-Client - Gabriel Rabelo" />
 
-      <AuthForm />
+      <AuthForm
+        handleLogin={handleLogin}
+        formDisabled={false}
+        setUserName={setUserName}
+        setPassword={setPassword}
+        formError={error?.message}
+      />
     </>
   );
 };
